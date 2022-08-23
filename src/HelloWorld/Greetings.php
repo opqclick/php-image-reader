05b1e06f3a6ac1d5a471972d072bce84cdb63b48
Md Abul Hasnat <opqclick@gmail.com>
2022-08-23 10:34:02 +0600

    staged

--- src/HelloWorld/Greetings.php
+++ src/HelloWorld/Greetings.php
@@ -1,9 +1,22 @@
 <?php
 
 namespace Opqclick\Imagereader\HelloWorld;
+use thiagoalessio\TesseractOCR\TesseractOCR;
+use thiagoalessio\TesseractOCR\TesseractOcrException;
 
 class Greetings {
-    public static function sayHelloWorld() {
-        return 'Hello World';
+    /**
+     * @throws TesseractOcrException
+     */
+    public static function sayHelloWorld(): string
+    {
+        $image = "./img/3.jpeg";
+
+        $content = new TesseractOCR($image);
+
+        if (str_contains($content->run(), 'name')) {
+            return true;
+        }
+
     }
 }
